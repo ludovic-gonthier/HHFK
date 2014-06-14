d160c27f23d950a47721e9f62bb91fa4a97fee8a
ludovic-gonthier <gonthier.ludovic@gmail.com>
2014-06-14 20:01:50 +0200

    First approach of INI File as configuration file

--- conf/routes.hh
+++ conf/routes.hh
@@ -7,3 +7,18 @@ $router = Router::getInstance();
 
 
 $router->provide("/", "Test\TestModule\Controller\TestController");
+
+/*
+[home]
+	pattern="/"
+	controller="Test\TestModule\Controller\TestController"
+	[home:options]
+		method="GET|POST"
+	[home:datas]
+		key="value"
+	[home:regexp]
+		variable_name="regexp_string"
+
+[user.profil]
+	pattern="/user/{user_id}/profile"
+*/
