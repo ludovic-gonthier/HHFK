25aa8b8728770e880382715c54c96d1253e2e908
ludovic-gonthier <gonthier.ludovic@gmail.com>
2014-06-27 00:03:48 +0200

    Develop the Debugger

--- vendor/HHFK/Debug/Debugger.hh
+++ vendor/HHFK/Debug/Debugger.hh
@@ -5,6 +5,16 @@ class Debugger
 {
 	public function dump(mixed $value) : void
 	{
-		echo "<pre>", var_dump($value); "</pre>";
+		echo "<pre>", \var_dump($value), "</pre>";
+	}
+
+	public function context() : void
+	{
+		echo "<pre>", \debug_backtrace(), "</pre>";
+	}
+
+	public function trace() : void
+	{
+		\debug_print_backtrace();
 	}
 }
