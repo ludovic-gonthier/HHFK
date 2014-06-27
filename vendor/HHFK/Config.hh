4578b16f874853b49b0d3ecd47c5c280724c7b57
ludovic-gonthier <gonthier.ludovic@gmail.com>
2014-06-27 20:52:54 +0200

    Server variable as a Config class

--- vendor/HHFK/Config.hh
+++ vendor/HHFK/Config.hh
@@ -7,37 +7,37 @@ class Config<T>
 	/**
 	 * Private constructor becasue the class is static
 	 */
-	private function __construct()
+	protected function __construct()
 	{}
 
 	public static function populate(array $config) : void
 	{
 		foreach ($config as $key => $value) {
-			self::$_configuration[$key] = $value;
+			static::$_configuration[$key] = $value;
 		}
 	}
 
 	public static function all() : ImmMap<string, T>
 	{
-		return self::$_configuration->toImmMap();
+		return static::$_configuration->toImmMap();
 	}
 
 	public static function exists(string $key) : bool
 	{
-		return self::$_configuration->contains($key);
+		return static::$_configuration->contains($key);
 	}
 
 	public static function get(string $key): ?T
 	{
-		if (self::$_configuration->contains($key)) {
-			return self::$_configuration->get($key);
+		if (static::$_configuration->contains($key)) {
+			return static::$_configuration->get($key);
 		}
 		return null;
 	}
 	public static function set(string $key, T $value) : void
 	{
-		self::$_configuration[$key] = $value;
+		static::$_configuration[$key] = $value;
 	}
 
-	private static Map<string, T> $_configuration = Map{};
+	protected static Map<string, T> $_configuration = Map{};
 }
--- vendor/HHFK/Server.hh
+++ vendor/HHFK/Server.hh
@@ -1,58 +1,10 @@
-<?hh //partial
+<?hh //strict
 namespace HHFK;
 
 /**
  * Class to encapsulate $_SERVER in HHVM
  */
-##TODO Serialize session
-class Server<T>
+class Server<T> extends Config
 {
-	public static function __callStatic(string $name, array $arguments = array()) : mixed
-	{
-		var_dump("lol???");die;
-		if (empty(self::$_server)) {
-			var_dump("lol???");
-			self::$_server = $_SERVER;
-		}
-		if (in_array($name, array("get", "set", "all"))) {
-			self::$name($arguments);
-		}
-		## TODO think if exception or false
-		return false;
-	}
-	/**
-	 * Set a variable session
-	 *
-	 * @param $key string The key to fetch
-	 * @return  T|null Return the value corresponding to the key or null if not found
-	 */
-	public static function get(mixed $key) : ?T
-	{
-		if (array_ket_exists($key, self::$_server)) {
-			return self::$_server[$key];
-		}
-		return null;
-	}
-	/**
-	 * Set a session variable
-	 * 
-	 * @param mixed $key   The key in which to store the variable
-	 * @param mixed $value The value to store in the Session
-	 */
-	public static function set(mixed $key, mixed $value) : void
-	{
-		self::$_server[$key] = $value;
-	}
-
-	/**
-	 * Fetch all the value of the $_SERVER variable
-	 * 
-	 * @return array server variables
-	 */
-	public static function all() : ?array
-	{
-		return self::$_server;
-	}
-
-	private static array $_server = $_SERVER;
+	protected static Map<string, T> $_configuration = Map{};
 }
